ff07f85234ded3abc7093848e757539ceb876361
dlwnsgus777 <dlwnsgus777@naver.com>
2024-09-06 09:31:55 +0900

    feat: FakeRepository 저장 로직 수정

--- practice/src/main/java/com/tdd/practice/account/model/domain/Account.java
+++ practice/src/main/java/com/tdd/practice/account/model/domain/Account.java
@@ -1,8 +1,10 @@
 package com.tdd.practice.account.model.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Account {
 	private Long id;
 	private String email;
