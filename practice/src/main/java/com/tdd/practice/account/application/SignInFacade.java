27319386aa1bb0622f52e5b917907f6bb9d42ad8
dlwnsgus777 <dlwnsgus777@naver.com>
2024-09-24 09:21:34 +0900

    feat: 이미 존재하는 회원이라면 에러 발생

--- practice/src/main/java/com/tdd/practice/account/application/SignInFacade.java
+++ practice/src/main/java/com/tdd/practice/account/application/SignInFacade.java
@@ -1,5 +1,7 @@
 package com.tdd.practice.account.application;
 
+import java.util.Optional;
+
 import com.tdd.practice.account.api.dto.SignInRequest;
 import com.tdd.practice.account.model.service.AccountService;
 import com.tdd.practice.account.model.service.out.AccountDto;
@@ -12,6 +14,11 @@ public class SignInFacade {
 	private final AccountService accountService;
 
 	public AccountDto execute(SignInRequest request) {
-		throw new UnsupportedOperationException("Not implemented yet");
+		Optional<AccountDto> findAccount = accountService.findByEmail(request.email());
+		if (findAccount.isPresent()) {
+			throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+		}
+
+		return null;
 	}
 }
--- practice/src/test/java/com/tdd/practice/application/SignInFacadeTest.java
+++ practice/src/test/java/com/tdd/practice/application/SignInFacadeTest.java
@@ -2,15 +2,30 @@ package com.tdd.practice.application;
 
 import com.tdd.practice.account.api.dto.SignInRequest;
 import com.tdd.practice.account.application.SignInFacade;
+import com.tdd.practice.account.infra.AccountRepository;
+import com.tdd.practice.account.model.domain.Account;
+import com.tdd.practice.account.model.service.AccountService;
+import com.tdd.practice.account.model.service.AccountServiceImpl;
+import com.tdd.practice.account.model.service.AccountServiceTest;
 import com.tdd.practice.account.model.service.out.AccountDto;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
 class SignInFacadeTest {
 	private SignInFacade signInFacade;
+	private AccountRepository accountRepository = new AccountServiceTest.FakeAccountRepository();
+
+	@BeforeEach
+	void setUp() {
+		AccountService accountService = new AccountServiceImpl(accountRepository);
+		this.signInFacade = new SignInFacade(accountService);
+	}
 
 	@Test
 	@DisplayName("회원가입 요청을 바탕으로 유저를 저장한다")
@@ -25,5 +40,24 @@ class SignInFacadeTest {
 
 		// THEN
 		assertThat(result.getEmail()).isEqualTo(email);
+	}
+
+	@Test
+	@DisplayName("이미 존재하는 회원이라면 오류가 발생한다")
+	public void testSignI_fail() {
+		// GIVEN
+		String password = "abcd!@#$";
+		String email = "test@test.com";
+		SignInRequest request = new SignInRequest(email, password);
+
+		accountRepository.save(Account.builder().email(email).build());
+
+		// WHEN
+		Throwable exception = assertThrows(RuntimeException.class, () -> {
+			signInFacade.execute(request);
+		});
+
+		// THEN
+		assertEquals("이미 존재하는 회원입니다.", exception.getMessage());
 	}
 }
