e0c932f28a03f3b8ab5edc3deaa847ac0d7e0016
dlwnsgus777 <dlwnsgus777@naver.com>
2024-10-05 12:50:01 +0900

    refactor: 코드 리팩터링

--- practice/src/main/java/com/tdd/practice/account/application/SignInFacade.java
+++ practice/src/main/java/com/tdd/practice/account/application/SignInFacade.java
@@ -3,7 +3,6 @@ package com.tdd.practice.account.application;
 import java.util.Optional;
 
 import com.tdd.practice.account.api.dto.SignInRequest;
-import com.tdd.practice.account.model.domain.Account;
 import com.tdd.practice.account.model.service.AccountService;
 import com.tdd.practice.account.model.service.in.SaveAccountInput;
 import com.tdd.practice.account.model.service.out.AccountDto;
@@ -18,17 +17,19 @@ public class SignInFacade {
 	private final PasswordEncoder passwordEncoder;
 
 	public AccountDto execute(SignInRequest request) {
-		Optional<AccountDto> findAccount = accountService.findByEmail(request.email());
-		if (findAccount.isPresent()) {
-			throw new IllegalArgumentException("이미 존재하는 회원입니다.");
-		}
-
-		SaveAccountInput input = new SaveAccountInput(request.email(), encodePassword(request));
-
+		checkExistsAccountByEmail(request.email());
+		SaveAccountInput input = new SaveAccountInput(request.email(), encodePassword(request.password()));
 		return accountService.save(input);
 	}
 
-	private String encodePassword(SignInRequest request) {
-		return passwordEncoder.encode(request.password());
+	private void checkExistsAccountByEmail(String email) {
+		Optional<AccountDto> findAccount = accountService.findByEmail(email);
+		if (findAccount.isPresent()) {
+			throw new IllegalArgumentException("이미 존재하는 회원입니다.");
+		}
+	}
+
+	private String encodePassword(String password) {
+		return passwordEncoder.encode(password);
 	}
 }
